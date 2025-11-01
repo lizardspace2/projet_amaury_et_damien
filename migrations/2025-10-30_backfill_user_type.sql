d2a8c00b3c3e69f6c38874d87bb654ebaf40985c
lizardspace2 <pierreduponddelabrouettet@proton.me>
2025-11-01 12:08:49 +0100

    ertrer

--- migrations/2025-10-30_backfill_user_type.sql
+++
@@ -1,9 +0,0 @@
--- Backfill des user_type manquants et sécurisation d'une valeur par défaut
-UPDATE public.profiles
-SET user_type = 'Particulier'
-WHERE user_type IS NULL;
-
--- Optionnel: si vous souhaitez normaliser certaines valeurs incohérentes (ex: pluriels/accents)
--- UPDATE public.profiles SET user_type = 'Professionnelle' WHERE user_type IN ('Professionnel', 'Pro');
-
-
